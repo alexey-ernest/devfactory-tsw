ab313b2e57326f9a3734f44b29eb0f8bbf0b3d75
Alexey Melnikov <alexey.melnikov@gteamstaff.com>
2016-06-22 16:41:13 +0700

    Favicon

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -16,7 +16,7 @@ import {WaypointDirective} from '../shared';
 export class FeaturesComponent {
   features: any[] = [
     { icon: 'important_devices', title: 'Crossbrowser Testing', description: 'Chrome, Safari, Firefox, IE, Edge' },
-    { icon: 'widgets', title: 'Docker 15 min Tooling', description: 'If you already have a docker image &mdash; its awesome, we can configure the tests even faster' },
+    { icon: 'widgets', title: 'Docker 15 min Tooling', description: 'If you already have a docker image - its awesome, we can configure the tests even faster' },
     { icon: 'looks_one', title: 'Automated Maintenance', description: 'Auto-healing tests and 1-click fixes make your life easier' }
   ];
 }
