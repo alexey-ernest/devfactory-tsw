a3b1cc4c1765326bc29a948b16e4911ea8020209
alexey-ernest <alexey.ernest@gmail.com>
2016-08-27 14:39:36 +0300

    News list animation

--- src/js/components/NewsListItem.react.js
+++ src/js/components/NewsListItem.react.js
@@ -3,11 +3,17 @@
  */
 
 import React, {Component, PropTypes} from 'react';
+import Waypoint from 'react-waypoint';
+import classNames from 'classnames';
 
 export default class NewsListItem extends Component {
 
   static propTypes = {
     news: PropTypes.object.isRequired
+  };
+
+  state = {
+    visible: false
   };
 
   render() {
@@ -16,11 +22,25 @@ export default class NewsListItem extends Component {
     // enable raw html
     let textHtml = { __html: news.text };
     return (
-      <li>
+      <li
+        className={classNames({
+          'animated': this.state.visible
+        })}
+        style={{
+          opacity: 0
+        }}
+        >
+        <Waypoint
+          onEnter={this._handleWaypointEnter}
+        />
         <h5>{news.title}</h5>
         <p dangerouslySetInnerHTML={textHtml}></p>
       </li>
     );
+  }
+
+  _handleWaypointEnter = () => {
+    this.setState({visible: true});
   }
 
 }
